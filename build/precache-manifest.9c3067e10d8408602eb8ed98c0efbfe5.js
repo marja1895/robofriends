46285dd041a2408e70a7e262c29675b95dfb6a80
Alice <marja1895@gmail.com>
2020-08-05 12:13:02 +0300

    downloading react Robofriends app

---
+++ build/precache-manifest.9c3067e10d8408602eb8ed98c0efbfe5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2666c5bfa9b1c52b551c9fed1be474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "aaf1697ab0e4d6189e65",
+    "url": "/static/css/main.15d86ffc.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf1697ab0e4d6189e65",
+    "url": "/static/js/main.1289bb0f.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "7a57849f3e246ab3a3982efd55ed13b0",
+    "url": "/static/media/teamgalaxy.7a57849f.woff"
+  }
+]);
---
+++ src/robots.js
@@ -0,0 +1,71 @@
+export const robots = [
+	{
+		id: 1,
+		name: "Kseniya Butyrska",
+		username: "Milashka",
+		email: "milashkak@mail.ru",
+	},
+
+	{
+		id: 2,
+		name: "Oksana Tyschenko",
+		username: "Ksyuna",
+		email: "oxik@mail.ru",
+	},
+
+	{
+		id: 3,
+		name: "Artyom Shevchenko",
+		username: "Sheva",
+		email: "shevak@rambler.ru",
+	},
+
+	{
+		id: 4,
+		name: "Gleb Turapov",
+		username: "Backbone",
+		email: "backbone@mail.ru",
+	},
+
+	{
+		id: 5,
+		name: "Rfterina Hayday",
+		username: "Kate25",
+		email: "kate25@mail.ru",
+	},
+
+	{
+		id: 6,
+		name: "Anton Tsymbal",
+		username: "Toxa",
+		email: "tosh63@mail.ru",
+	},
+
+	{
+		id: 7,
+		name: "Tatyana Hovtra",
+		username: "Taty",
+		email: "taty91@mail.ru",
+	},
+
+	{
+		id: 8,
+		name: "Ann Pylypas",
+		username: "Aneta",
+		email: "aneta21@gmail.com",
+	},
+
+	{
+		id: 9,
+		name: "Andrey Smith",
+		username: "MiDroid",
+		email: "drue56@yahoo.com",
+	},
+
+	{
+		id: 10,
+		name: "Ivan Maskov",
+		username: "Elon",
+		email: "m.elon@gmail.com",
+	},
+];
